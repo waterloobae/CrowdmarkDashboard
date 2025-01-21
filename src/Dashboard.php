58c8c26d7f01a0e6544cfa7ea15acea7266cee0c
Terry Bae <thbae@uwaterloo.ca>
2025-01-21 08:08:38 -0500

    ==== Logger Implementation Starts ====

--- src/Dashboard.php
+++ src/Dashboard.php
@@ -7,6 +7,15 @@ if (session_status() === PHP_SESSION_NONE) {
 use Waterloobae\CrowdmarkDashboard\API;
 
 class Dashboard{
+    private object $logger;
+    private object $crowdmark;
+
+    public function __construct(){
+        // constructor
+        $this->logger = new Logger();
+        $this->crowdmark = new Crowdmark();
+    }
+
     public function getData($name) {
         return array("status" => "success", "data" => "Hello, $name! This is data from the server!");
     }
