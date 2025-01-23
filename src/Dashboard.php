8eddf2329dd7f67f3fb3465ed195890c4ea2ac6b
Terry Bae <thbae@uwaterloo.ca>
2025-01-23 14:04:56 -0500

    Remove unused API import and add validation comments in Dashboard class

--- src/Dashboard.php
+++ src/Dashboard.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-use Waterloobae\CrowdmarkDashboard\API;
+// use Waterloobae\CrowdmarkDashboard\API;
 
 class Dashboard{
     private object $logger;
@@ -16,10 +16,12 @@ class Dashboard{
         $this->crowdmark = new Crowdmark();
     }
 
+    // Validations
+    // 1. API_KEY.php exists
+    // 2. $api_key is set
+    // 3. API returns 200 response
+
     public function getData($name) {
         return array("status" => "success", "data" => "Hello, $name! This is data from the server!");
     }
 }
-
-
-
