f69e11c1cbb9d2b06286bb2acee362a52b2fa0ce
Terry Bae <thbae@uwaterloo.ca>
2025-02-19 14:43:43 -0500

    Enhance error handling in render method of Engine class to provide clearer view not found message

--- src/Engine.php
+++ src/Engine.php
@@ -6,9 +6,8 @@ class Engine
     public function render(string $viewsName, array $data = []): string
     {
         $path = __DIR__ . '/views/' . $viewsName . '.php';
-        die($path);
         if (!file_exists($path)) {
-            throw new \InvalidArgumentException('View not found');
+            die('View ['.$path.'] not found');
         }
         $contents = file_get_contents($path);
         foreach ($data as $key => $value) {
