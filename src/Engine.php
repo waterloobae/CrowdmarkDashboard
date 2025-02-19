fc0d45ee5bf439f4df1852f98967eb2670be2f54
Terry Bae <thbae@uwaterloo.ca>
2025-02-19 14:39:57 -0500

    Add debug output for view path in render method of Engine class

--- src/Engine.php
+++ src/Engine.php
@@ -6,6 +6,10 @@ class Engine
     public function render(string $viewsName, array $data = []): string
     {
         $path = __DIR__ . '/views/' . $viewsName . '.php';
+        die($path);
+        if (!file_exists($path)) {
+            throw new \InvalidArgumentException('View not found');
+        }
         $contents = file_get_contents($path);
         foreach ($data as $key => $value) {
 
